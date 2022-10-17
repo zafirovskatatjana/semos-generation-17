47c5e5bc685e1d398a245e8e41ab1f6048c56517
Cvetan Dimeski <dimeski.cvetan@gmail.com>
2022-10-17 22:30:30 +0200

    implemented custom expiry date logic

--- handlers/auth/index.js
+++ handlers/auth/index.js
@@ -6,19 +6,35 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 
 const {validate, validateCreateNewAccountRule, validateLoginRule} = require('./validator');
+const { jwt_secret_key: JWT_SECRET, expiryTime } = config.getConfigPropertyValue("security");
 
 
 const MILISECONDS = 1000;
+const ONE_SECOND = 1; // in seconds
 const ONE_MINUTE = 60; // in seconds
 const ONE_HOUR = ONE_MINUTE * 60; // in minutes
 const ONE_DAY = ONE_HOUR * 24; // in hours
+// default to 1d expiration of token if it's not set in the config
+
+const timePeriodDictionary = {
+    'd': ONE_DAY,
+    'm': ONE_MINUTE,
+    's': ONE_SECOND,
+    'h': ONE_HOUR
+}
+
+const calculateExpiryTime = () => {
+    const timePeriod = timePeriodDictionary[expiryTime.charAt(expiryTime.length - 1)];
+    const numberOfTime = expiryTime.substring(0, expiryTime.length - 1); // TODO: change bad name
+    return timePeriod * numberOfTime;
+}
+const TIME_TO_LIVE = expiryTime != null ? calculateExpiryTime() : ONE_DAY;
 
 const BAD_REQUEST_STATUS = 400; // HTTP status
 const NOT_FOUND_STATUS = 404; // HTTP status
 const OK_STATUS = 200; // HTTP status
 const CREATED_STATUS = 201; // HTTP status
 
-const { jwt_secret_key: JWT_SECRET } = config.getConfigPropertyValue("security");
 
 const login = async (request, response) => {
     try {
@@ -41,18 +57,17 @@ const login = async (request, response) => {
                 status: BAD_REQUEST_STATUS,
                 message: `Passwords don't match`
             }
-        };
+        }
         
         const payloadData = {
             username: account.username,
             email: account.email,
             exp: getExpiryDateForToken()
         };
-        
 
-        const token = jwt.sign(payloadData, JWT_SECRET);
+        const encodedToken = jwt.sign(payloadData, JWT_SECRET);
+        return response.status(OK_STATUS).send({ token: encodedToken });
 
-        return response.status(OK_STATUS).send({token});
     } catch (err) {
         // return the bad request when we have an error
         return response.status(err.status).send(err.message);
@@ -106,7 +121,7 @@ const forgotPassword = (request, response) => {};
 const resetPassword = (request, response) => {};
 
 
-const getExpiryDateForToken = () => Math.floor(new Date().getTime() / MILISECONDS * ONE_DAY);
+const getExpiryDateForToken = () => Math.floor(new Date().getTime() / MILISECONDS + TIME_TO_LIVE);
 
 module.exports = {
     login,
